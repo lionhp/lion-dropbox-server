afd520dc469d10fa103899db8295273208530d22
hp <123456>
2019-03-07 14:20:28 +0800

    init project

--- README.md
+++ README.md
@@ -1,32 +1,23 @@
 # lion-dropbox-server
-This is a dropbox server written by golang, based  on webdav protocol.
+This is a dropbox server written by golang, based on webdav protocol.
 
-## Introduction - 介绍
+## Introduction
+This is a dropbox server written by golang, based on webdav protocol. If you need dropbox client, please go to my other dropbox client project. Currently this project focus on achieving not performance.
 
-### Summary - 概要
+## Requirements
+- beego (go get github.com/astaxie/beego)
 
-### Features - 特性
+## Usage
+1. Modify configuration file(config.ini)
+2. `go build main.go`
+3. Upload to your server
+4. Use linux systemd to manage the server.
 
-## Requirements - 必要条件
+## Changelog
 
-## Configuration - 配置
 
-## Installation - 安装
-
-## Usage - 用法
-
-## Development - 开发
-
-## Changelog - 更新日志
-
-## FAQ - 常见问题
-
-## Support - 支持
-
-### Dos - 文档（更多文档。）
-
-### Contact - 联系（其他联系信息（电子邮件地址，网站，公司名称，地址等）。提交bug，功能要求，提交补丁，加入邮件列表，得到通知，或加入用户或开发开发区群的介绍。）
-
-## Authors and acknowledgment
+### Contact
+Email: lionhanpeng@163.com
 
 ## License 
+If you like, you can do what you want to do.
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"github.com/astaxie/beego/config"
+	"golang.org/x/net/context"
+	"golang.org/x/net/webdav"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	conf, err := config.NewConfig("ini", "./config.ini")
+	if err != nil {
+		log.Println("read config failed, err:", err)
+		return
+	}
+	port := conf.String("server::port")
+	uname := conf.String("user::name")
+	upwd := conf.String("user::pwd")
+	root := conf.String("root::path")
+
+	fs := &webdav.Handler{
+		FileSystem: webdav.Dir(root),
+		LockSystem: webdav.NewMemLS(),
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		username, pwd, ok := r.BasicAuth()
+		if r.Method != "MKCOL" && !ok {
+			w.Header().Set("Content-Type", "text/html;charset=utf-8")
+			w.Header().Set("WWW-Authenticate", `Basic realm=" Lion Cloud"`)
+			w.WriteHeader(401)
+			return
+		}
+		if r.Method != "MKCOL" && (username != uname || pwd != upwd) {
+			http.Error(w, "Account/Password Error", 401)
+			return
+		}
+
+		switch r.Method {
+		case "PUT", "DELETE", "PROPPATCH", "MKCOL", "COPY", "MOVE":
+			http.Error(w, "Your request method have no Permission", 403)
+			return
+		}
+		if r.Method == "GET" && handleDirList(fs.FileSystem, w, r) {
+			return
+		}
+		fs.ServeHTTP(w, r)
+	})
+
+	http.ListenAndServe(":"+port, nil)
+	//http.ListenAndServeTLS(":"+port, "")
+}
+
+func handleDirList(fs webdav.FileSystem, w http.ResponseWriter, r *http.Request) bool {
+	ctx := context.Background()
+
+	f, err := fs.OpenFile(ctx, r.URL.Path, os.O_RDONLY, 0)
+
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	if fi, _ := f.Stat(); fi != nil && !fi.IsDir() {
+		return false
+	}
+
+	dirs, err := f.Readdir(-1)
+	if err != nil {
+		log.Print(w, "Read file error", 500)
+		return false
+	}
+
+	w.Header().Set("Content-Type", "text/html;charset=utf-8")
+	fmt.Fprintf(w, "<pre>\n")
+	for _, d := range dirs {
+		name := d.Name()
+		if d.IsDir() {
+			name += "/"
+		}
+		fmt.Fprintf(w, "<a href=\"%s\">%s</a>\n", name, name)
+	}
+	fmt.Fprintf(w, "</pre>\n")
+	return true
+}
